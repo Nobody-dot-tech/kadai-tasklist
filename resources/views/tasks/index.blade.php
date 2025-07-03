1f13c89db2309bb50efdda3f50c77f5c94ed9cf3
Nobody <kangerlong0@gmail.com>
2025-07-03 12:38:51 +0900

    task controller view_index

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="prose ml-4">
+    <h2 class="text-lg">タスク 一覧</h2>
+  </div>
+
+  @if (isset($tasks))
+    <table class="table table-zebra w-full my-4">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>タスク</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($tasks as $task)
+          <tr>
+            <td><a class="link link-hover text-info" href="#">{{ $task->id }}</a></td>
+            <td>{{ $task->content }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  @endif
+
+
+@endsection
