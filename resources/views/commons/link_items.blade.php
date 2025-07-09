df0d3d3b978c34fa30db9e23fe17fa93f7674886
Nobody <kangerlong0@gmail.com>
2025-07-09 20:12:24 +0900

    link_items-tasks.show_Auth

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -2,7 +2,7 @@
     {{-- 新規タスク追加ページへのリンク --}}
     <li><a class="link link-hover" href="{{ route('tasks.create') }}">新規タスクの追加</a></li>
     {{-- タスク一覧ページへのリンク --}}
-    <li><a class="link link-hover" href="{{ route('tasks.show') }}">{{ Auth::user()->name }}のタスク一覧</a></li>
+    <li><a class="link link-hover" href="{{ route('tasks.show', Auth::user()->id }}">{{ Auth::user()->name }}のタスク一覧</a></li>
     {{-- ログアウトへのリンク --}}
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
 @else
